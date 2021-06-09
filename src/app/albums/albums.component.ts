07959ede9af43444e87a449c418d2960fe582993
peteroneill-ct <85463543+peteroneill-ct@users.noreply.github.com>
2021-06-09 19:19:58 -0400

    Set up dummy data

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IAlbumDetails } from '../interfaces'
+
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumsComponent implements OnInit {
 
-  constructor() { }
+  albumDetails: IAlbumDetails
+  constructor() {
+    //Dummy data
+    this.albumDetails = {
+      artist: 'Artist name',
+      name: 'Album name',
+      image: 'assets/img/testImg.jpg'
+    } as IAlbumDetails
+
+  }
 
   ngOnInit(): void {
   }
