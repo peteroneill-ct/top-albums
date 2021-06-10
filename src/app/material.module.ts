388f55de9cf107406074d5ce1298c339da7b2916
peteroneill-ct <85463543+peteroneill-ct@users.noreply.github.com>
2021-06-10 15:46:15 -0400

    Add ripple effect

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
+import { MatRippleModule } from '@angular/material/core'
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
-const modules = [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule]
+const modules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatRippleModule,
+]
 
 @NgModule({
   declarations: [],
